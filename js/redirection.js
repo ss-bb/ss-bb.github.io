b3fe5e4fb906da23ef5fab0e21bea296837960ef
olivier <goyon.olivier@gmail.com>
2014-07-18 10:39:54 +0200

    file+redirection
    ajout des fichier pour tester les redirection avec github

---
+++ js/redirection.js
@@ -0,0 +1,15 @@
+/**
+ * Created by olivier on 18/07/14.
+ */
+
+//DEFI ACCEPTER : METTRE UN GOTO :).
+function Goto(valeur){
+    var debuturl = window.location.hostname;
+    switch(valeur){
+        case 1:window.location=debuturl+"/Professionnel.html";break;
+        case 2:window.location=debuturl+"/Piano.html";break;
+        case 3:window.location=debuturl+"/News.html";break;
+        default: windows.location=debuturl;
+    }
+}
+
