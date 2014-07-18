b530252ecca42e53d1b6d76b992b160e630a077f
olivier <goyon.olivier@gmail.com>
2014-07-18 10:42:16 +0200

    changement origin
    changement pour redirection

--- js/redirection.js
+++ js/redirection.js
@@ -4,7 +4,7 @@
 
 //DEFI ACCEPTER : METTRE UN GOTO :).
 function Goto(valeur){
-    var debuturl = window.location.hostname;
+    var debuturl = window.location.origin;
     switch(valeur){
         case 1:window.location=debuturl+"/Professionnel.html";break;
         case 2:window.location=debuturl+"/Piano.html";break;
